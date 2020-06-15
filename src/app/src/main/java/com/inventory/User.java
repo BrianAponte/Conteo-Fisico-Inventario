1a1768f370b0eb5f706f04d9d6bc5cd5115a8b5e
BrianAponte <54904230+BrianAponte@users.noreply.github.com>
2020-06-14 19:19:25 -0500

    Revert "Estructura modificada para orden"
    This reverts commit f7fc538c615b79f9a5e383e00925c28dc23d08d6.

---
+++ src/app/src/main/java/com/inventory/User.java
@@ -0,0 +1,15 @@
+package com.inventory;
+
+public class User {
+
+    int id;
+    String name;
+    String pass;
+
+    public User(int id, String name, String pass) {
+        this.id = id;
+        this.name = name;
+        this.pass = pass;
+    }
+
+}
