e096a080abbc66447ea10b97eb8ed1706f78bb59
As1nt <56849040+As1nt@users.noreply.github.com>
2020-06-15 21:20:26 -0500

    Casi terminada pantalla crear artículos

--- src/app/src/main/java/com/inventory/Create_art.java
+++ src/app/src/main/java/com/inventory/Create_art.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Create_art extends AppCompatActivity {
     String user_n;
@@ -16,7 +18,44 @@ public class Create_art extends AppCompatActivity {
         user_n = intent.getStringExtra("user_name");
     }
 
-    public void addProd(View v) {}
+    public void addProd(View v) {
+        art_management a_m = art_management.getInstance();
+        TextView success = findViewById(R.id.success_art);
+        TextView error = findViewById(R.id.error_art);
+        String sku = ((EditText) findViewById(R.id.new_sku)).getText().toString();
+        String name = ((EditText) findViewById(R.id.new_prod)).getText().toString();
+        String category = ((EditText) findViewById(R.id.new_category)).getText().toString();
+        String stock = ((EditText) findViewById(R.id.new_stock)).getText().toString();
+        String count = ((EditText) findViewById(R.id.new_count)).getText().toString();
+        if(!sku.matches("")&&!name.matches("")&&!category.matches("")&&!stock.matches("")&&!count.matches("")) {
+            Product newProd = new Product(sku, name, category, Integer.parseInt(stock), Integer.parseInt(count));
+            if(a_m.amount_of_art==0) {
+                a_m.add_art(newProd);
+                error.setVisibility(View.INVISIBLE);
+                success.setText("Producto añadido, actualmente hay "+a_m.amount_of_art +" productos");
+                success.setVisibility(View.VISIBLE);
+            }
+            else {
+                Product productFound = a_m.findProd(newProd);
+                if(productFound==null||!productFound.name.matches(name)) {
+                    a_m.add_art(newProd);
+                    error.setVisibility(View.INVISIBLE);
+                    success.setText("Producto añadido, actualmente hay "+a_m.amount_of_art +" productos");
+                    success.setVisibility(View.VISIBLE);
+                }
+                else {
+                    success.setVisibility(View.INVISIBLE);
+                    error.setText("Nombre de artículo ya registrado");
+                    error.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+        else {
+            success.setVisibility(View.INVISIBLE);
+            error.setText("Rellene todos los campos");
+            error.setVisibility(View.VISIBLE);
+        }
+    }
 
     public void go_back(View v){
         Intent i = new Intent(this, DisplayMessageActivity.class);
